711acfc6e1abc7cccd94dbd37f285fd643985d92
Samuel Simões <slip.cm@gmail.com>
2015-05-03 22:25:03 -0300

    Added the mixin feature on Flux namespace

--- src/fluxo.js
+++ src/fluxo.js
@@ -59,8 +59,43 @@
       var callbacks = this.events[eventName] || {};
 
       for (var subscriptionId in callbacks) {
-        callbacks[subscriptionId].apply(null, Array.prototype.slice.call(arguments, 1))
+        callbacks[subscriptionId].apply(null, Array.prototype.slice.call(arguments, 1));
       }
+    }
+  };
+
+  Fluxo.Mixin = function(toMix) {
+    var mixins = Array.prototype.slice.call(arguments, 1);
+
+    var aggregateProperty = function(propName, property) {
+      var existingProp = toMix[propName];
+
+      toMix[propName] = function () {
+        var args = Array.prototype.slice.call(arguments);
+
+        property.apply(toMix, args);
+
+        return existingProp.apply(toMix, args);
+      };
+    };
+
+    var mix = function(mixin) {
+      for (var propName in mixin) {
+        var property = mixin[propName];
+
+        if (toMix.hasOwnProperty(propName) && typeof(property) === "function") {
+          aggregateProperty(propName, property);
+        } else if (typeof(property) === "function") {
+          toMix[propName] = property.bind(toMix);
+        } else {
+          toMix[propName] = property;
+        }
+      }
+    };
+
+    for (var i = 0, l = mixins.length; i < l; i ++) {
+      var mixin = mixins[i];
+      mix(mixin);
     }
   };
 
