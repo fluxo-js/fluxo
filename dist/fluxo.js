b74cc258fccf172c6b400e287feac720081c52a8
Samuel Simões <slip.cm@gmail.com>
2015-06-21 14:35:39 -0300

    v0.0.12

--- dist/fluxo.js
+++ dist/fluxo.js
@@ -1,4 +1,4 @@
-/*! fluxo v0.0.11 | (c) 2014, 2015 Samuel Simões |  */
+/*! fluxo v0.0.12 | (c) 2014, 2015 Samuel Simões |  */
 (function(root, factory) {
   if (typeof define === "function" && define.amd) {
     define([], factory);
@@ -474,54 +474,6 @@ Fluxo.callAction = function(actionHandlerIdentifier, actionName) {
   var args = Array.prototype.slice.call(arguments, 2);
 
   return action.apply(handler, args);
-};
-
-
-  Fluxo.ConnectStores = function (Component, stores) {
-  return React.createClass({
-    storesOnChangeCancelers: [],
-
-    getInitialState: function() {
-      return this.storesState();
-    },
-
-    storesState: function() {
-      var state = {};
-
-      for (var storeName in stores) {
-        var store = stores[storeName];
-        state[storeName] = store.toJSON();
-      }
-
-      return state;
-    },
-
-    updateComponentState: function() {
-      this.setState(this.storesState);
-    },
-
-    componentWillMount: function() {
-      for (var storeName in stores) {
-        var store = stores[storeName];
-        this.listenStore(store);
-      }
-    },
-
-    listenStore: function(store) {
-      var canceler = store.on(["change"], this.updateComponentState.bind(this));
-      this.storesOnChangeCancelers.push(canceler);
-    },
-
-    componentWillUnmount: function() {
-      for (var i = 0, l = this.storesOnChangeCancelers.length; i < l; i ++) {
-        this.storesOnChangeCancelers[i].call();
-      }
-    },
-
-    render: function() {
-      return React.createElement(Component, Fluxo.extend({}, this.props, this.state));
-    }
-  });
 };
 
 
