e1d340c25113e0d4b1fe7a0130d9975a4272f29e
Samuel Simões <slip.cm@gmail.com>
2015-07-15 00:08:46 -0300

    Build dist

--- dist/fluxo.js
+++ dist/fluxo.js
@@ -444,38 +444,5 @@ Fluxo.CollectionStore = Fluxo.Base.extend(
 });
 
 
-  Fluxo.actionHandlers = {};
-
-Fluxo.registerActionHandler = function(identifier, handler) {
-  handler = Fluxo.extend({}, handler);
-
-  Fluxo.actionHandlers[identifier] = handler;
-
-  var args = Array.prototype.slice.call(arguments, 2);
-
-  if (typeof handler.initialize == "function") {
-    handler.initialize.apply(handler, args);
-  }
-};
-
-Fluxo.callAction = function(actionHandlerIdentifier, actionName) {
-  var handler = Fluxo.actionHandlers[actionHandlerIdentifier];
-
-  if (!handler) {
-    throw new Error("Action handler " + actionHandlerIdentifier + " not found.");
-  }
-
-  var action = handler[actionName];
-
-  if (!action) {
-    throw new Error("Action " + actionName + " not found on " + actionHandlerIdentifier + " handler.");
-  }
-
-  var args = Array.prototype.slice.call(arguments, 2);
-
-  return action.apply(handler, args);
-};
-
-
   return Fluxo;
 });
