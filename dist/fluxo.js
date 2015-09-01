5c674654c69ccdbfe607322be7b5652bced3054f
Samuel Simões <slip.cm@gmail.com>
2015-09-01 19:34:12 -0300

    v0.0.16

--- dist/fluxo.js
+++ dist/fluxo.js
@@ -1,4 +1,4 @@
-/*! fluxo v0.0.15 | (c) 2014, 2015 Samuel Simões |  */
+/*! fluxo v0.0.16 | (c) 2014, 2015 Samuel Simões |  */
 (function(root, factory) {
   if (typeof define === "function" && define.amd) {
     define([], factory);
