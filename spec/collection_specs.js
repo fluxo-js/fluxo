f126334e1435eec4363a41628b1bc304fe68b5ab
Samuel Simões <slip.cm@gmail.com>
2015-09-05 10:17:48 -0300

    Removing unnecessary contexts on specs

--- spec/collection_specs.js
+++ spec/collection_specs.js
@@ -1,150 +1,148 @@
 describe("Fluxo.CollectionStore", function () {
-  context("on an instance", function() {
-    it("parsing store's data on collection's store object", function() {
-      var collection = Fluxo.CollectionStore.create({
-        stores: [
-          { name: "Fluxo" }
-        ],
+  it("parsing store's data on collection's store object", function() {
+    var collection = Fluxo.CollectionStore.create({
+      stores: [
+        { name: "Fluxo" }
+      ],
 
-        store: {
-          customMethod: function() {
-            return this.data.name + "foo";
-          }
+      store: {
+        customMethod: function() {
+          return this.data.name + "foo";
         }
-      });
-
-      expect(collection.stores[0].customMethod()).to.be.eql("Fluxofoo");
+      }
     });
 
-    it("#cid", function() {
-      var collection1 = Fluxo.CollectionStore.create(),
-          collection2 = Fluxo.CollectionStore.create();
+    expect(collection.stores[0].customMethod()).to.be.eql("Fluxofoo");
+  });
 
-      expect(collection1.cid).to.exist;
-      expect(collection2.cid).to.exist;
+  it("#cid", function() {
+    var collection1 = Fluxo.CollectionStore.create(),
+        collection2 = Fluxo.CollectionStore.create();
 
-      expect(collection1.cid).to.not.equal(collection2.cid);
+    expect(collection1.cid).to.exist;
+    expect(collection2.cid).to.exist;
+
+    expect(collection1.cid).to.not.equal(collection2.cid);
+  });
+
+  it("#addStore", function() {
+    var collection = Fluxo.CollectionStore.create(),
+        onChangeCallback = chai.spy();
+
+    collection.on(["change"], onChangeCallback);
+
+
+    var store = collection.addStore({ name: "Samuel" });
+
+    expect(collection.toJSON()).to.be.eql({
+      data: { cid: collection.cid },
+      stores: [{ cid: store.cid, name: "Samuel" }]
     });
+    expect(onChangeCallback).to.have.been.called();
+  });
 
-    it("#addStore", function() {
-      var collection = Fluxo.CollectionStore.create(),
-          onChangeCallback = chai.spy();
+  it("calls onChangeCallback when a child store changes", function() {
+    var collection = Fluxo.CollectionStore.create({ stores: [{ name: "Samuel" }] }),
+        onChangeCallback = chai.spy(),
+        onStoreNameChangeCallback = chai.spy();
 
-      collection.on(["change"], onChangeCallback);
+    collection.on(["stores:change"], onChangeCallback);
+    collection.on(["stores:change:name"], onStoreNameChangeCallback);
 
+    collection.stores[0].setAttribute("name", "Samuel S");
+    expect(onChangeCallback).to.have.been.called();
+    expect(onStoreNameChangeCallback).to.have.been.called();
+  });
 
-      var store = collection.addStore({ name: "Samuel" });
+  it("#remove", function() {
+    var store = Fluxo.ObjectStore.create({ data: { name: "Samuel" } }),
+        collection = Fluxo.CollectionStore.create(),
+        onChangeCallback = chai.spy();
 
-      expect(collection.toJSON()).to.be.eql({
-        data: { cid: collection.cid },
-        stores: [{ cid: store.cid, name: "Samuel" }]
-      });
-      expect(onChangeCallback).to.have.been.called();
-    });
+    collection.addStore(store);
 
-    it("calls onChangeCallback when a child store changes", function() {
-      var collection = Fluxo.CollectionStore.create({ stores: [{ name: "Samuel" }] }),
-          onChangeCallback = chai.spy(),
-          onStoreNameChangeCallback = chai.spy();
+    collection.remove(store);
 
-      collection.on(["stores:change"], onChangeCallback);
-      collection.on(["stores:change:name"], onStoreNameChangeCallback);
+    collection.on(["change"], onChangeCallback);
 
-      collection.stores[0].setAttribute("name", "Samuel S");
-      expect(onChangeCallback).to.have.been.called();
-      expect(onStoreNameChangeCallback).to.have.been.called();
-    });
+    store.setAttribute("name", "a diferent name");
 
-    it("#remove", function() {
-      var store = Fluxo.ObjectStore.create({ data: { name: "Samuel" } }),
-          collection = Fluxo.CollectionStore.create(),
-          onChangeCallback = chai.spy();
+    expect(collection.stores).to.be.eql([]);
+    expect(onChangeCallback).to.not.have.been.called();
+  });
 
-      collection.addStore(store);
+  it("#removeAll", function() {
+    var collection = Fluxo.CollectionStore.create({ stores: [{ name: "Samuel" }, { name: "Fluxo" }] }),
+        onChangeCallback = chai.spy();
 
-      collection.remove(store);
+    collection.on(["change"], onChangeCallback);
 
-      collection.on(["change"], onChangeCallback);
+    collection.removeAll();
 
-      store.setAttribute("name", "a diferent name");
+    expect(collection.stores).to.be.eql([]);
+    expect(onChangeCallback).to.have.been.called();
+  });
 
-      expect(collection.stores).to.be.eql([]);
-      expect(onChangeCallback).to.not.have.been.called();
-    });
+  it("#addStores", function() {
+    var collection = Fluxo.CollectionStore.create(),
+        store = Fluxo.ObjectStore.create(),
+        onChangeCallback = chai.spy();
 
-    it("#removeAll", function() {
-      var collection = Fluxo.CollectionStore.create({ stores: [{ name: "Samuel" }, { name: "Fluxo" }] }),
-          onChangeCallback = chai.spy();
+    collection.on(["change", "add"], onChangeCallback);
 
-      collection.on(["change"], onChangeCallback);
+    collection.addStores([store]);
 
-      collection.removeAll();
+    expect(collection.stores).to.be.eql([store]);
+    expect(onChangeCallback).to.have.been.called.exactly(2);
+  });
 
-      expect(collection.stores).to.be.eql([]);
-      expect(onChangeCallback).to.have.been.called();
-    });
+  it("#where", function() {
+    var collection = Fluxo.CollectionStore.create();
 
-    it("#addStores", function() {
-      var collection = Fluxo.CollectionStore.create(),
-          store = Fluxo.ObjectStore.create(),
-          onChangeCallback = chai.spy();
+    var store1 = Fluxo.ObjectStore.create({ data: { id: 20, name: "samuel" } }),
+        store2 = Fluxo.ObjectStore.create({ data: { id: 21, name: "simoes" } }),
+        store3 = Fluxo.ObjectStore.create({ data: { id: 22, name: "simoes" } });
 
-      collection.on(["change", "add"], onChangeCallback);
+    collection.addStores([store1, store2, store3]);
 
-      collection.addStores([store]);
+    expect(collection.where({ name: "simoes" })).to.be.eql([store2, store3]);
+    expect(collection.findWhere({ name: "samuel" })).to.be.eql(store1);
+  });
 
-      expect(collection.stores).to.be.eql([store]);
-      expect(onChangeCallback).to.have.been.called.exactly(2);
-    });
+  it("#sort", function() {
+    var collection = Fluxo.CollectionStore.create();
 
-    it("#where", function() {
-      var collection = Fluxo.CollectionStore.create();
+    collection.sort = function(a, b) {
+      return a.data.price - b.data.price;
+    };
 
-      var store1 = Fluxo.ObjectStore.create({ data: { id: 20, name: "samuel" } }),
-          store2 = Fluxo.ObjectStore.create({ data: { id: 21, name: "simoes" } }),
-          store3 = Fluxo.ObjectStore.create({ data: { id: 22, name: "simoes" } });
+    var store1 = Fluxo.ObjectStore.create({ data: { price: 100 } }),
+        store2 = Fluxo.ObjectStore.create({ data: { price: 10 } }),
+        store3 = Fluxo.ObjectStore.create({ data: { price: 1 } });
 
-      collection.addStores([store1, store2, store3]);
+    collection.addStores([store1, store2, store3]);
 
-      expect(collection.where({ name: "simoes" })).to.be.eql([store2, store3]);
-      expect(collection.findWhere({ name: "samuel" })).to.be.eql(store1);
-    });
+    expect(collection.stores).to.be.eql([store3, store2, store1]);
+  });
 
-    it("#sort", function() {
-      var collection = Fluxo.CollectionStore.create();
+  it("#setStores", function() {
+    var collection = Fluxo.CollectionStore.create();
 
-      collection.sort = function(a, b) {
-        return a.data.price - b.data.price;
-      };
+    var store1 = Fluxo.ObjectStore.create({ data: { id: 1, name: "Samuel" } });
 
-      var store1 = Fluxo.ObjectStore.create({ data: { price: 100 } }),
-          store2 = Fluxo.ObjectStore.create({ data: { price: 10 } }),
-          store3 = Fluxo.ObjectStore.create({ data: { price: 1 } });
+    collection.addStore(store1);
 
-      collection.addStores([store1, store2, store3]);
+    collection.setStores([{ id: 1, name: "Simões" }, { id: 2, name: "Foo" }]);
 
-      expect(collection.stores).to.be.eql([store3, store2, store1]);
-    });
+    expect(store1.data.name).to.be.eql("Simões");
+    expect(collection.stores).to.be.eql([store1, collection.find(2)]);
+  });
 
-    it("#setStores", function() {
-      var collection = Fluxo.CollectionStore.create();
+  it("#find", function() {
+    var collection = Fluxo.CollectionStore.create(),
+        store = collection.addStore({ id: 1 });
 
-      var store1 = Fluxo.ObjectStore.create({ data: { id: 1, name: "Samuel" } });
-
-      collection.addStore(store1);
-
-      collection.setStores([{ id: 1, name: "Simões" }, { id: 2, name: "Foo" }]);
-
-      expect(store1.data.name).to.be.eql("Simões");
-      expect(collection.stores).to.be.eql([store1, collection.find(2)]);
-    });
-
-    it("#find", function() {
-      var collection = Fluxo.CollectionStore.create(),
-          store = collection.addStore({ id: 1 });
-
-      expect(collection.find(store.cid)).to.equal(store);
-      expect(collection.find(store.data.id)).to.equal(store);
-    });
+    expect(collection.find(store.cid)).to.equal(store);
+    expect(collection.find(store.data.id)).to.equal(store);
   });
 });
--- spec/object_store_specs.js
+++ spec/object_store_specs.js
@@ -1,88 +1,86 @@
 describe("Fluxo.ObjectStore", function () {
-  context("on an instance", function() {
-    it("#cid", function() {
-      var store1 = Fluxo.ObjectStore.create(),
-          store2 = Fluxo.ObjectStore.create();
+  it("#cid", function() {
+    var store1 = Fluxo.ObjectStore.create(),
+        store2 = Fluxo.ObjectStore.create();
 
-      expect(store1.cid).to.exist;
-      expect(store2.cid).to.exist;
+    expect(store1.cid).to.exist;
+    expect(store2.cid).to.exist;
 
-      expect(store1.cid).to.not.equal(store2.cid);
+    expect(store1.cid).to.not.equal(store2.cid);
+  });
+
+  it("#setAttribute", function() {
+    var store = Fluxo.ObjectStore.create(),
+        onChangeCallback = chai.spy(),
+        onChangeNameCallback = chai.spy();
+
+    store.on(["change"], onChangeCallback);
+    store.on(["change:name"], onChangeNameCallback);
+
+    store.setAttribute("name", "Samuel");
+
+    expect(store.data).to.be.eql({ name: "Samuel" });
+    expect(onChangeCallback).to.have.been.called();
+    expect(onChangeNameCallback).to.have.been.called();
+  });
+
+  it("#set", function() {
+    var store = Fluxo.ObjectStore.create({ data: { name: "Samuel" } }),
+        onChangeCallback = chai.spy(),
+        onChangeNameCallback = chai.spy();
+
+    expect(store.data).to.be.eql({ name: "Samuel" });
+
+    store.on(["change"], onChangeCallback);
+    store.on(["change:name"], onChangeNameCallback);
+
+    store.set({ name: "Other", email: "fluxo@flux.com" });
+
+    expect(store.data).to.be.eql({ name: "Other", email: "fluxo@flux.com" });
+    expect(onChangeCallback).to.have.been.called();
+    expect(onChangeNameCallback).to.have.been.called();
+  });
+
+  it("#toJSON", function() {
+    var store = Fluxo.ObjectStore.create({ data: { name: "Samuel" } });
+    expect(store.toJSON()).to.be.eql({ cid: store.cid, name: "Samuel" });
+  });
+
+  it("computed attributes", function() {
+    var store = Fluxo.ObjectStore.create({
+      data: {
+        first_name: "Samuel",
+        last_name: "Simoes"
+      },
+
+      computed: {
+        "fullName": ["change:first_name", "change:last_name"]
+      },
+
+      fullName: function() {
+        return (this.data.first_name + " " + this.data.last_name);
+      }
     });
 
-    it("#setAttribute", function() {
-      var store = Fluxo.ObjectStore.create(),
-          onChangeCallback = chai.spy(),
-          onChangeNameCallback = chai.spy();
+    expect(store.data.fullName).to.be.eql("Samuel Simoes");
 
-      store.on(["change"], onChangeCallback);
-      store.on(["change:name"], onChangeNameCallback);
+    store.setAttribute("first_name", "Neo");
 
-      store.setAttribute("name", "Samuel");
+    expect(store.data.fullName).to.be.eql("Neo Simoes");
+  });
 
-      expect(store.data).to.be.eql({ name: "Samuel" });
-      expect(onChangeCallback).to.have.been.called();
-      expect(onChangeNameCallback).to.have.been.called();
-    });
+  it("attributes parser", function() {
+    var store = Fluxo.ObjectStore.create({
+      data: { count: "1" },
 
-    it("#set", function() {
-      var store = Fluxo.ObjectStore.create({ data: { name: "Samuel" } }),
-          onChangeCallback = chai.spy(),
-          onChangeNameCallback = chai.spy();
-
-      expect(store.data).to.be.eql({ name: "Samuel" });
-
-      store.on(["change"], onChangeCallback);
-      store.on(["change:name"], onChangeNameCallback);
-
-      store.set({ name: "Other", email: "fluxo@flux.com" });
-
-      expect(store.data).to.be.eql({ name: "Other", email: "fluxo@flux.com" });
-      expect(onChangeCallback).to.have.been.called();
-      expect(onChangeNameCallback).to.have.been.called();
-    });
-
-    it("#toJSON", function() {
-      var store = Fluxo.ObjectStore.create({ data: { name: "Samuel" } });
-      expect(store.toJSON()).to.be.eql({ cid: store.cid, name: "Samuel" });
-    });
-
-    it("computed attributes", function() {
-      var store = Fluxo.ObjectStore.create({
-        data: {
-          first_name: "Samuel",
-          last_name: "Simoes"
-        },
-
-        computed: {
-          "fullName": ["change:first_name", "change:last_name"]
-        },
-
-        fullName: function() {
-          return (this.data.first_name + " " + this.data.last_name);
+      attributeParsers: {
+        count: function(value) {
+          return parseInt(value, 10);
         }
-      });
-
-      expect(store.data.fullName).to.be.eql("Samuel Simoes");
-
-      store.setAttribute("first_name", "Neo");
-
-      expect(store.data.fullName).to.be.eql("Neo Simoes");
+      }
     });
 
-    it("attributes parser", function() {
-      var store = Fluxo.ObjectStore.create({
-        data: { count: "1" },
-
-        attributeParsers: {
-          count: function(value) {
-            return parseInt(value, 10);
-          }
-        }
-      });
-
-      expect(store.data.count).to.be.eql(1);
-    });
+    expect(store.data.count).to.be.eql(1);
   });
 
   it("#triggerEvent", function() {
